32e66bb89a9f93060645bc8f78e191d9f5aef805
Fergal Moran <fergal.moran@gmail.com>
2023-01-08 00:03:34 +0000

    Remove dark mode for now

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  // darkMode: 'class',
   content: [
     './node_modules/flowbite-react/**/*.js',
     './app/**/*.{js,ts,jsx,tsx}',
