16805a00a34f0ab5a0d3adfc9b71ee8477a54a8d
Fergal Moran <fergal.moran@gmail.com>
2024-10-21 22:49:29 +0100

    Shitty login form added

---
+++ src/components/forms/auth/login-form.tsx
@@ -0,0 +1,73 @@
+"use client";
+import React from "react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+
+const LoginForm: React.FC = () => {
+  const formSchema = z.object({
+    email: z.string().email({
+      message: "Invalid email.",
+    }),
+    password: z.string().min(8, {
+      message: "Password must be at least 8 characters.",
+    }),
+  });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "fergal.moran+mixyboos@gmail.com",
+      password: "SVqVKJWZh5dIaM7JsNY1h0E/xbzPCD7y7Veedxa1Q/k=",
+    },
+  });
+
+  const onSubmit = form.handleSubmit((data) => {
+    console.log(data);
+  });
+  return (
+    <Form {...form}>
+      <form onSubmit={onSubmit} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Email address" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="Password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default LoginForm;
