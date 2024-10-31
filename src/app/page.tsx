449c29a2422b47cba875d07a4459b2cd708298f5
Fergal Moran <fergal.moran@gmail.com>
2024-10-31 18:36:18 +0000

    50% done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,17 @@ import LoginForm from "@/components/forms/auth/login-form";
 import DebugPage from "@/components/pages/debug";
 import HeroPage from "@/components/pages/hero-page";
 import { useAuth } from "@/lib/contexts/auth/auth-context";
+import { redirect } from "next/navigation";
 
 export default function Home() {
   const { profile } = useAuth();
+
+  if (profile) {
+    redirect("/dashboard");
+  }
   return (
     <div className="m-28">
-      {profile ? <DebugPage /> : <HeroPage />}
-
-      <div>{profile?.name}</div>
+      <HeroPage />
     </div>
   );
 }
