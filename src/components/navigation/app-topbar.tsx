f0b54638d2d704dcb65f797b851296c58386216e
Fergal Moran <fergal.moran@gmail.com>
2025-04-01 20:00:18 +0100

    Top bar improvements

--- src/components/navigation/app-topbar.tsx
+++ src/components/navigation/app-topbar.tsx
@@ -6,6 +6,8 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 import { ThemeSelector } from "@/components/theme/theme-toggle";
 import React from "react";
 import { ModeToggle } from "@/components/theme/mode-toggle";
+import { Icons } from "@/components/icons";
+import Link from "next/link";
 export function AppTopbar() {
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
@@ -15,16 +17,24 @@ export function AppTopbar() {
           orientation="vertical"
           className="mx-2 data-[orientation=vertical]:h-4"
         />
-        <h1 className="text-base font-medium">Documents</h1>
+
+        <h1 className="text-base font-medium">
+          <Button variant="outline" size="sm" className="mr-2" asChild>
+            <Link href="/live/create">
+              <Icons.liveStream className="mr-1.5 h-4 w-4" />
+              Go Live
+            </Link>
+          </Button>
+        </h1>
         <div className="ml-auto flex items-center gap-2">
-          <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
+          <Button variant="ghost" size="icon" asChild className="hidden sm:flex">
             <a
-              href="https://github.com/shadcn-ui/ui/tree/main/apps/v4/app/(examples)/dashboard"
+              href="https://github.com/mixyboos/"
               rel="noopener noreferrer"
               target="_blank"
-              className="dark:text-foreground"
             >
-              GitHub
+              <Icons.github className="h-5 w-5" />
+              <span className="sr-only">GitHub</span>
             </a>
           </Button>
           <ThemeSelector />
--- src/components/navigation/nav-me.tsx
+++ src/components/navigation/nav-me.tsx
@@ -42,11 +42,11 @@ export function NavMe() {
         <SidebarMenu>
           <SidebarMenuItem className="flex items-center gap-2">
             <SidebarMenuButton
-              tooltip="Upload Show"
+              tooltip="Upload Mix"
               className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
             >
               <IconCirclePlusFilled />
-              <span>Upload Show</span>
+              <span>Upload Mix</span>
             </SidebarMenuButton>
             <Button
               size="icon"
